f0d58ed481678221adb4dcf5d68449b442ef8eb5
Narek <narek2799@gmail.com>
2024-07-31 01:07:52 +0400

    create slider2

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,19 +1,34 @@
-"use strict"
+/** @format */
+
+"use strict";
 
 const slider = document.querySelector(".main__slider");
 const images = document.querySelectorAll(".slider__img");
 const sliderLeft = document.querySelector(".sliderLeft");
 const sliderRight = document.querySelector(".sliderRight");
 
-
+let slideNum = 0;
 sliderLeft.addEventListener("click", () => {
-    for (let i of images) {
-      i.style.translate = "0%"
-   }
-})
+  slideNum--;
+
+  for (let i of images) {
+      if (slideNum < 0) {
+       slideNum = 0
+      i.style.translate = "0%";
+    } else i.style.translate = `${slideNum * -100}% `;
+  }
+});
 
 sliderRight.addEventListener("click", () => {
-    for (let i of images) {
-      i.style.translate = "-100%"
-   }
-})
+  slideNum++;
+  console.log(slideNum);
+  for (let i of images) {
+      if (slideNum > images.length - 1) {
+          slideNum = 0
+       i.style.translate = "0%" ;
+      } else {
+          i.style.translate = `${slideNum * -100}% `
+          console.log(slideNum);
+      }
+  }
+});
