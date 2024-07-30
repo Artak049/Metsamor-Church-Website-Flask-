910f72c118303acc5aa11ee8c0b1426d4f30cbc7
Artak049 <artakaxper020@gmail.com>
2024-07-30 16:40:32 +0400

    adding some functional 'specially daily_word function'

--- app/models/DailyWord.py
+++ app/models/DailyWord.py
@@ -5,5 +5,5 @@ from ..extensions import db
 class DailyWord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     book = db.Column(db.String(20), nullable=False)
-    chapter = db.Column(db.String(100), nullable=False)
+    chapter = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
--- app/routes/DailyWord.py
+++ app/routes/DailyWord.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from ..extensions import db
-from ..models import DailyWord
+from ..models.DailyWord import DailyWord
 
 daily_word = Blueprint('daily_word', __name__)
 
 
-@daily_word.route('/admin', methods=['GET', 'POST'])
+@daily_word.route('/admin/create', methods=['GET', 'POST'])
 def daily_word_admin():
-    return render_template('admin.html')
+    if request.method == 'POST':
+        book = request.form['book-name']
+        chapter = request.form['chapter']
+
+        daily_word = DailyWord(book=book, chapter=chapter)
+
+        try:
+            db.session.add(daily_word)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(e)
+    else:
+        return render_template('admin.html')
